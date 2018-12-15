e3395b43b731edc6c773db57fb73052c277a8549
Junbin Zhang <34506631+wolf-bailang@users.noreply.github.com>
2018-12-15 21:04:44 +0800

    Add files via upload

--- TensorFlow Technology Analysis and Actual Warfare _Li Jiaxuan/4.9.2 QueueRunner线程管理器.py
+++ TensorFlow Technology Analysis and Actual Warfare _Li Jiaxuan/4.9.2 QueueRunner线程管理器.py
@@ -18,3 +18,27 @@ with tf.Session() as sess:
     for i in range(10):
     print (sess.run(q.dequeue()))
 
+#使用协调器（coordinator）来管理线程
+#主线程
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
+
+#Coordinator: 协调器，协调线程间的关系可以视为一种信号量，用来做同步
+coord = tf.train.Coordinator()
+
+#启动入队线程，协调器是线程的参数
+enqueue_threads = qr.create_threads(sess, coord = coord, start = True)
+
+coord.request_stop() #通知其他线程关闭
+#主线程
+for i in range(0, 10):
+    #使用tf.errors.OutOfRangeError 来捕捉错误，终止循环
+    try:
+        print(sess.run(q.dequeue()))
+    except tf.errors.OutOfRangeError:
+        break
+
+coord.join(enqueue_threads) #join操作等待其他线程结束，其他所有线程关闭之后，这一函数才能返回
+
+
+
