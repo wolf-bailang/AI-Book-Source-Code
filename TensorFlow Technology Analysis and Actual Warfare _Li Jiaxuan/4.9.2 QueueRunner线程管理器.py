8a87bd4e19d20bfa4e1314b9e9b22c0bdbe970e0
Junbin Zhang <34506631+wolf-bailang@users.noreply.github.com>
2018-12-15 20:23:41 +0800

    Add files via upload

--- TensorFlow Technology Analysis and Actual Warfare _Li Jiaxuan/4.9.2 QueueRunner线程管理器.py
+++ TensorFlow Technology Analysis and Actual Warfare _Li Jiaxuan/4.9.2 QueueRunner线程管理器.py
@@ -9,3 +9,12 @@ enqueue_op = q.enqueue(counter) #操作：计数器值加入队列
 #创建一个队列管理器QueueRunner
 qr = tf.train.QueueRunner(q, enqueue_ops=[increment_op, enqueue_op] * 1)
 
+#启动一个会话，从队列管理器qr 中创建线程
+#主线程
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    enqueue_threads = qr.create_threads(sess, start=True) # 启动入队线程
+    #主线程
+    for i in range(10):
+    print (sess.run(q.dequeue()))
+
